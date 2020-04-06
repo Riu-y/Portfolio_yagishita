18d9523f626d620eabf925c657f94e441c0eb19d
Riu-y <y1a1r2y5u@gmail.com>
2020-04-06 05:54:42 +0000

    Finish Profile&Edit page,Add top page

--- app/controllers/drivers/transfer_informations_controller.rb
+++ app/controllers/drivers/transfer_informations_controller.rb
@@ -1,7 +1,14 @@
 class Drivers::TransferInformationsController < ApplicationController
   def index
+  	@transfer_informations = current_driver.transfer_infomarions
+  	@transfer_information = TransferInformation.new
+
   end
 
   def edit
   end
+
+  private
+  def transfer_information_params
+  	params.require(:transfer_information).permit(:driver_id, :bank_name, :branch_name, :account_type,:account_number,:account_name)
 end
