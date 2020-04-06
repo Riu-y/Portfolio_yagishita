f1db679d4eb2eb44427a281106e8551799d30247
Riu-y <y1a1r2y5u@gmail.com>
2020-04-06 05:54:42 +0000

    Finish Profile&Edit page,Add top page

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,7 +1,23 @@
 class DriversController < ApplicationController
-  def index
+  def show
+  	@driver = current_driver
   end
 
   def edit
+  	@driver = Driver.find(params[:id])
+  end
+
+  def update
+  	@driver = Driver.find(params[:id])
+  	if @driver.update(driver_params)
+  		redirect_to drivers_root_path(@driver)
+  	else
+  		render :edit
+  	end
+  end
+
+  private
+  def driver_params
+  	params.require(:driver).permit(:driver_name,:driver_name_kana, :profile_image, :postal_code, :address, :telephone_number)
   end
 end
--- app/models/transfer_information.rb
+++ app/models/transfer_information.rb
@@ -1,2 +1,10 @@
 class TransferInformation < ApplicationRecord
+	validates :bank_name,, presence: true
+	validates :branch_name, presence: true
+	validates :account_type, presence: true
+	validates :account_number, presence: true
+	validates :account_name, presence: true
+
+
+	belongs_to :driver
 end
