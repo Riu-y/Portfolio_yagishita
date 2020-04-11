84333460f1a8569ce59196e46adaa587d308ffb1
Riu-y <y1a1r2y5u@gmail.com>
2020-04-11 13:03:24 +0000

    Enroute Ajax function

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,8 @@
 class FavoritesController < ApplicationController
 	def create
-		ad = Ad.find(params[:ad_id])
+		@ad = Ad.find(params[:ad_id])
 		favorite = current_driver.favorites.new(ad_id: ad.id)
 		favorite.save
-		redirect_back(fallback_location: ads_path)
 	end
 
 
@@ -11,6 +10,5 @@ class FavoritesController < ApplicationController
 		ad = Ad.find(params[:ad_id])
 		favorite = current_driver.favorites.find_by(ad_id: ad.id)
 		favorite.destroy
-		redirect_back(fallback_location: ads_path)
 	end
 end
