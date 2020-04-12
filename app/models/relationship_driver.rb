9e78917006f627dbdc22140c623a81cd27aafbf5
Riu-y <y1a1r2y5u@gmail.com>
2020-04-12 13:40:21 +0000

    Enroute follow function(Mutual bottuns for driver and ad_client are done)

--- app/models/relationship_driver.rb
+++ app/models/relationship_driver.rb
@@ -1,6 +1,6 @@
 class RelationshipDriver < ApplicationRecord
-	belongs_to :follower, class_name: "Driver"
-	belongs_to :followed, class_name: "Driver"
+	belongs_to :follower, class_name: "AdClient"
+	belongs_to :followed, class_name: "AdClient"
 
 	validates :follower_id,presence: true
 	validates :followed_id,presence: true
