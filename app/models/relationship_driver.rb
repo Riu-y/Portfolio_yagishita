8fe257614f2e2c05cd50946a8cfae6272dc3a63a
Riu-y <y1a1r2y5u@gmail.com>
2020-04-13 13:51:18 +0000

    Finish follow function

--- app/models/relationship_driver.rb
+++ app/models/relationship_driver.rb
@@ -1,5 +1,5 @@
 class RelationshipDriver < ApplicationRecord
-	belongs_to :follower, class_name: "AdClient"
+	belongs_to :follower, class_name: "Driver"
 	belongs_to :followed, class_name: "AdClient"
 
 	validates :follower_id,presence: true
