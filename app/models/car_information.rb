784c8bea88f059671ac9fb0207cc9bb6cb437a9c
Riu-y <y1a1r2y5u@gmail.com>
2020-04-21 12:03:05 +0000

    Finish Japanese translation

--- app/models/car_information.rb
+++ app/models/car_information.rb
@@ -1,9 +1,13 @@
 class CarInformation < ApplicationRecord
 	validates :manufacturer_name, presence: true
 	validates :car_name, presence: true
-	validates :certificate_image, presence: true
 	validates :flame_number, presence: true
 	validates :license_plate, presence: true
+	validates :certificate_image, presence: {
+		message: -> (rec, data){
+			I18n.t('activemodel.errors.message.not_upload')
+		}
+	}
 
 	belongs_to :driver
 
