7d0d99692fc14c5d9ec84fc7c2079df80bb11ab2
Riu <y1a1r2y5u@gmail.com>
2020-08-12 23:25:23 +0900

    [modify]config.yml_32

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,7 +21,6 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with(:truncation)
     Rails.application.load_seed
   end
-end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
