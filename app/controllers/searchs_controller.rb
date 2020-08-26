3dce5cee988925cc3353f822307c58c6e076e711
Riu-y <y1a1r2y5u@gmail.com>
2020-08-26 17:57:13 +0000

    [Edit]検索機能の使用変更

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -2,9 +2,9 @@ class SearchsController < ApplicationController
 	#ドライバー側 検索アクション
 	def search_driver
 		@driver= current_driver
-		@content = params["search"]["content"]
-		@search = search_for_driver(@content)
-		@records = @search.page
+		content = params["search"]["content"]
+		@ad_clients = AdClient.where( 'company_name LIKE ?', '%'+content+'%' ).page
+		@ads = Ad.where('title LIKE ?', '%'+content+'%').page
 	end
 	#広告主側 検索アクション
 	def search_ad_client
@@ -14,27 +14,8 @@ class SearchsController < ApplicationController
 		@records = @search.page
 	end
 	#検索機能
-  private
+  	private
 	def search_for_ad_client(content)
 		Driver.where('driver_name LIKE ? OR address LIKE ?','%'+content+'%', '%'+content+'%' )
 	end
-	def search_for_driver(content)
-		AdClient.joins(:ad).where('ad.title LIKE ?', '%'+content+'%')
-		# Ad.where('title LIKE ?', '%'+content+'%')
-		# User.joins(:group).where('users.name = "hanako" AND groups.name = "hobby"')
-
-	end
-	
 end
-
-	# def search_for(model, content)
-	# 	if model == 'ad'
-	# 		Ad.where('title LIKE ?', '%'+content+'%')
-	# 	elsif model == 'ad_client'
-	# 		AdClient.where('company_name LIKE ?', '%'+content+'%')
-	# 	elsif  model == 'driver'
-	# 		Driver.where('driver_name LIKE ?', '%'+content+'%')
-	# 	elsif model == 'address'
-	# 		Driver.where('address LIKE ?', '%'+content+'%')
-	# 	end
-	# end
